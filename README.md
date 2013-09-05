4ba418c5606e492a422773d536f0bd82ed945f09
Qiu Jian <qiujian@meituan.com>
2013-09-05 15:03:54 +0800

    Initialization

--- README.md
+++ README.md
@@ -2,3 +2,29 @@ ovs_upgrade
 ===========
 
 Tools upgrade openvswitch without service interruption
+
+Usage
+-----
+
+* ovs_install.sh
+
+    Install both kernel and userspace software
+
+
+* ovs_control
+
+    Uitility script to restart the services and kernel module
+
+* download.sh
+
+    Utility to download ovs source codes
+
+
+Example
+-------
+
+    sh download.sh
+
+    ./ovs_install.sh 1.10.0
+
+    sudo ./ovs_control force-reload-kmod
---
+++ ovs_install.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+
+VERSION=$1
+
+if test -z $VERSION; then
+    echo "Usage: ovs_install.sh <version>"
+    exit 1
+fi
+
+PACKAGE=openvswitch-$VERSION
+
+if test ! -e $PACKAGE.tar.gz; then
+    echo "$PACKAGE.tar.gz not exists"
+    exit 1
+fi
+
+rm -fr $PACKAGE
+tar xf $PACKAGE.tar.gz
+
+cd $PACKAGE
+if test -f /etc/redhat-release; then
+    RELEASE=centos
+else
+    RELEASE=ubuntu
+fi
+if test -e ../patches/$RELEASE/$PACKAGE; then
+    patch -p1 -i ../patches/$RELEASE/$PACKAGE/openvswitch.patch
+fi
+MODULE_DIR=/lib/modules/`uname -r`
+if test ! -f ./configure; then
+    ./boot.sh
+    if test "$?" -ne "0"; then
+        echo "./boot.sh failed"
+        exit 1
+    fi
+fi
+./configure --with-linux=$MODULE_DIR/build --enable-ndebug --prefix=/usr/local
+if test "$?" -ne "0"; then
+    echo "./configure failed"
+    exit 1
+fi
+make
+if test "$?" -ne "0"; then
+    echo "Compile failed"
+    exit 1
+fi
+sudo PATH=/usr/local/bin:$PATH make install
+KMOD_PATH=datapath/linux/openvswitch_mod.ko
+if test ! -f $KMOD_PATH; then
+    KMOD_PATH=datapath/linux/openvswitch.ko
+    if test ! -f $KMOD_PATH; then
+        echo "Fail to compile kmod"
+        exit 1
+    fi
+fi
+MODULE_DEST=$MODULE_DIR/kernel/net/openvswitch
+sudo mkdir -p $MODULE_DEST
+sudo rm -fr $MODULE_DEST/*
+sudo cp $KMOD_PATH $MODULE_DEST
+sudo depmod -a
+
+if test ! -f /usr/local/etc/openvswitch/conf.db; then
+    sudo mkdir -p /usr/local/etc/openvswitch
+    if test -f /etc/openvswitch/conf.db; then
+        sudo cp /etc/openvswitch/* /usr/local/etc/openvswitch
+        if test "$RELEASE" == "ubuntu"; then
+            sudo apt-get -y remove --purage openvswitch-switch
+            sudo apt-get -y autoremove --purge
+        fi
+    else
+        sudo /usr/local/bin/ovsdb-tool create /usr/local/etc/openvswitch/conf.db vswitchd/vswitch.ovsschema
+    fi
+fi
