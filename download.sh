1156c16c304fda4de2853adc8f6f38a4a6f88912
Qiu Jian <qiujian@meituan.com>
2014-06-28 07:14:01 +0800

    Add support for 2.0.1 and 2.1.2

--- download.sh
+++ download.sh
@@ -3,3 +3,5 @@ wget http://openvswitch.org/releases/openvswitch-1.7.3.tar.gz
 wget http://openvswitch.org/releases/openvswitch-1.9.3.tar.gz
 wget http://openvswitch.org/releases/openvswitch-1.10.0.tar.gz
 wget http://openvswitch.org/releases/openvswitch-1.11.0.tar.gz
+wget http://openvswitch.org/releases/openvswitch-2.0.1.tar.gz
+wget http://openvswitch.org/releases/openvswitch-2.1.2.tar.gz
